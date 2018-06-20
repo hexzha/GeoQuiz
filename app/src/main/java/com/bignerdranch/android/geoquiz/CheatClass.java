2ff575e871ae171470542000b4b7af7ba6d952c9
hexzha <sam10.zhang@gmail.com>
2018-06-19 23:12:06 -0500

    Add cheat button to activity_quiz.xml

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatClass.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatClass extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
